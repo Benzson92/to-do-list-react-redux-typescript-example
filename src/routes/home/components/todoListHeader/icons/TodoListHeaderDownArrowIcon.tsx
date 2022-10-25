01018caaba10013dac2974c2b4437b88198db3bc
benzson92 <benzson.v@gmail.com>
2022-10-26 00:08:24 +0700

    icons

---
+++ src/routes/home/components/todoListHeader/icons/TodoListHeaderDownArrowIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TodoListHeaderDownArrowIcon: React.FunctionComponent = () => {
+  return (
+    <svg
+      viewBox="0 0 8 8"
+      fill="currentColor"
+      width="1em"
+      height="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6.50423 2.4209L3.99998 4.92515L1.49573 2.4209L0.670898 3.24573L3.99998 6.57482L7.32907 3.24573L6.50423 2.4209Z" />
+    </svg>
+  );
+};
+
+export default TodoListHeaderDownArrowIcon;
---
+++ src/routes/home/components/todoListTasks/icons/TodoListTaskCheckIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TodoListTaskCheckIcon: React.FunctionComponent = () => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      width="1em"
+      height="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6.08333 10.0401L3.75079 7.70759L2.74921 8.70917L6.08333 12.0433L12.9591 5.16751L11.9575 4.16592L6.08333 10.0401Z" />
+    </svg>
+  );
+};
+
+export default TodoListTaskCheckIcon;
