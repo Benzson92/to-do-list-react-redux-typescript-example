44c5f24ae2845c1eec0b9e46651a400a5cc8bb14
benzson92 <benzson.v@gmail.com>
2022-10-26 00:10:23 +0700

    Todo List Header Filter

---
+++ src/routes/home/components/todoListHeader/TodoListHeaderFilter.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+import { TaskType } from '../../../../types/TaskType';
+
+import TodoListHeaderDownArrowIcon from './icons/TodoListHeaderDownArrowIcon';
+
+import styles from './TodoListHeaderFilter.module.scss';
+
+type Props = {
+	isDisabled: boolean;
+	currentValue: TaskType;
+	setCurrentValue: React.Dispatch<React.SetStateAction<TaskType>>;
+};
+
+const TodoListHeaderFilter: React.FunctionComponent<Props> = ({
+	isDisabled,
+	currentValue,
+	setCurrentValue,
+}) => {
+	const [open, setOpen] = useState(false);
+
+	const handleToggleSelectButton = () => {
+		setOpen((prevState) => !prevState);
+	};
+
+	const handleValueChange = (value: TaskType) => {
+		setCurrentValue(value);
+	};
+
+	const handleChange = (value: TaskType) => {
+		handleValueChange(value);
+		handleToggleSelectButton();
+	};
+
+	return (
+		<div
+			className={`${styles.TodoListHeaderFilter} ${
+				isDisabled ? 'disabled' : ''
+			}`}
+		>
+			<button
+				disabled={isDisabled}
+				onClick={handleToggleSelectButton}
+				className={styles.SelectButton}
+			>
+				{currentValue ? currentValue : TaskType.All}
+				<div
+					title="Down Arrow Icon"
+					className={`${styles.TodoListHeaderDownArrowIcon} ${
+						open ? styles.ActiveTodoListHeaderDownArrowIcon : ''
+					}`}
+				>
+					<TodoListHeaderDownArrowIcon />
+				</div>
+			</button>
+			{open && (
+				<div className={styles.DropdownMenu}>
+					{Object.values(TaskType).map((value) => {
+						const isActive = value === currentValue;
+
+						return (
+							<div
+								key={value}
+								onClick={() => handleChange(value)}
+								className={`${styles.DropdownMenuItem} ${
+									isActive ? styles.ActiveDropdownMenuItem : ''
+								}`}
+							>
+								{value}
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default TodoListHeaderFilter;
