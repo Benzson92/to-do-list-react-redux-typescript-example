113f7f03470aeb23d9115fc07294e9672ced243e
benzson92 <benzson.v@gmail.com>
2022-10-26 00:09:05 +0700

    Todo List Task Item

---
+++ src/routes/home/components/todoListTasks/TodoListTaskItem.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { TaskDTO } from '../../../../models/TaskDTO';
+
+import TodoListTaskCheckIcon from './icons/TodoListTaskCheckIcon';
+
+import styles from './TodoListTaskItem.module.scss';
+
+type Props = {
+	task: TaskDTO;
+	onClickIsCompletedTaskItem: () => void;
+	renderTaskItemMenu: () => React.ReactElement;
+};
+
+const TodoListTaskItem: React.FunctionComponent<Props> = ({
+	task: { title, completed },
+	onClickIsCompletedTaskItem,
+	renderTaskItemMenu,
+}) => {
+	return (
+		<div className={styles.TodoListTaskItem}>
+			<div className={styles.TodoListTaskLeftContent}>
+				<div
+					onClick={onClickIsCompletedTaskItem}
+					title="Checkbox"
+					className={`${styles.Checkbox} ${
+						completed ? styles.CheckboxCompleted : ''
+					}`}
+				>
+					{completed && <TodoListTaskCheckIcon />}
+				</div>
+				<div
+					data-testid="TodoListTaskItemTitle"
+					className={`${styles.Title} ${
+						completed ? styles.TitleCompleted : ''
+					}`}
+				>
+					{title}
+				</div>
+			</div>
+			{renderTaskItemMenu()}
+		</div>
+	);
+};
+
+export default TodoListTaskItem;
